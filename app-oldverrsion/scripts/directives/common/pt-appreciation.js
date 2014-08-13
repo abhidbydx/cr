32354f3dc553c238f3e274792d5000106ab9eee4
soumya <soumyapandey2013@gmail.com>
2014-08-13 18:21:20 +0530

    delete unwanted folder

--- app-oldverrsion/scripts/directives/common/pt-appreciation.js
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-angular.module('serviceApp').directive('ptAppreciation',function(){
-	return {
-		restrict: 'A',
-		scope: { rate: '=', time: '=' },
-		templateUrl: 'views/directives/common/pt-appreciation.html'
-	}
-});
